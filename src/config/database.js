5db4779bfb2f0605d2b124a2cc73f403a20dfccf
Victor Tumialan <victortumialan@multiplica.com>
2022-02-13 01:51:08 -0500

    Fix env

--- src/config/database.js
+++ src/config/database.js
@@ -2,24 +2,24 @@ require('dotenv').config();
 
 module.exports = {
   "development": {
-    "username": process.env.USER,
-    "password": process.env.PASSWORD,
-    "database": process.env.DATABASE,
-    "host": process.env.HOST,
+    "username": "z7Fg42k2j1",
+    "password": "mjKjizEWD2",
+    "database": "z7Fg42k2j1",
+    "host": "remotemysql.com",
     "dialect": "mysql"
   },
   "test": {
-    "username": process.env.USER,
-    "password": process.env.PASSWORD,
-    "database": process.env.DATABASE,
-    "host": process.env.HOST,
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
     "dialect": "mysql"
   },
-  "uction": {
-    "username": process.env.USER,
-    "password": process.env.PASSWORD,
-    "database": process.env.DATABASE,
-    "host": process.env.HOST,
+  "production": {
+    "username": process.env.DBUSER,
+    "password": process.env.DBPASSWORD,
+    "database": process.env.DBDATABASE,
+    "host": process.env.DBHOST,
     "dialect": "mysql"
   }
 };
